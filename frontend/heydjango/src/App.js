8c61aaa87207c3e42af5f50f5c2aa8d53989bb17
Thiago <74683757+ThiagoShow@users.noreply.github.com>
2022-06-17 10:01:35 +0200

    imports

---
+++ frontend/heydjango/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { w3cwebsocket as W3CWebSocket } from "websocket";
+
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import Paper from '@material-ui/core/Paper';
+import Avatar from '@material-ui/core/Avatar';
+import { WithStyles } from '@material-ui/core/styles/withStyles';
